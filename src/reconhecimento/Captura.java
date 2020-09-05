1b3b122f24549ee024046da992fef2886a382cf3
Felipe Barros <felipe92barros@gmail.com>
2020-09-05 12:27:08 -0300

    Salvando fotos capturadas

--- src/reconhecimento/Captura.java
+++ src/reconhecimento/Captura.java
@@ -3,8 +3,11 @@ package reconhecimento;
 import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
 import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
 import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import static org.bytedeco.opencv.global.opencv_imgproc.resize;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 
 import java.awt.event.KeyEvent;
+import java.util.Scanner;
 
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
@@ -20,7 +23,7 @@ import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
 
 public class Captura {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception, InterruptedException {
 
 		KeyEvent tecla = null;
 
@@ -37,7 +40,15 @@ public class Captura {
 		Frame frameCapturado = null;
 
 		Mat imagemColorida = new Mat();
+		
+		int numeroAmostrasImagem = 25;
+		
+		int amostra = 1;
 
+		System.out.print("Digite seu ID: ");
+		Scanner cadastro = new Scanner(System.in);
+		int idPessoa = cadastro.nextInt();
+		
 		while ((frameCapturado = camera.grab()) != null) {
 
 			imagemColorida = converteMat.convert(frameCapturado);
@@ -50,13 +61,42 @@ public class Captura {
 			
 			detectorFaces.detectMultiScale(imagemCinza, facesDesctadas, 1.1, 1, 0, new Size(150, 150), new Size(500, 500));
 			
+			if(tecla == null) {
+				tecla = cFrame.waitKey(5);
+			}
+			
 			for (int i = 0; i < facesDesctadas.size(); i++) {
 				Rect dadosFaces = facesDesctadas.get(0);
 				rectangle(imagemColorida, dadosFaces, new Scalar(0,0,255,0));
+				Mat faceCapturada = new Mat(imagemCinza, dadosFaces);
+				resize(faceCapturada, faceCapturada, new Size(160, 160));
+				
+				if(tecla == null) {
+					tecla = cFrame.waitKey(5);
+				}
+				
+				if(tecla != null) {
+					if(tecla.getKeyChar() == 'q') {
+						if(amostra <= numeroAmostrasImagem) {
+							imwrite("src\\fotos\\pessoa." + idPessoa + "." + amostra + ".jpg", faceCapturada);
+							System.out.println("Foto: " + amostra + " Capturada\n");
+							amostra++;
+						}
+					}
+					tecla = null;
+				}
+			}
+			
+			if(tecla == null) {
+				tecla = cFrame.waitKey(20);
 			}
 
 			if (cFrame.isVisible()) {
 				cFrame.showImage(frameCapturado);
+			}
+			
+			if(amostra > numeroAmostrasImagem) {
+				break;
 			}
 		}
 
